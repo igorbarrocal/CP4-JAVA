8e979d5670d16d0c45977f321d44ef1ee1aa65a3
igorbarrocal <igorbarrocal@hotmail.com>
2025-09-05 10:33:30 -0300

    feat: implementa ProjetoDTO e ProjetoController com CRUD básico de projetos

--- AULA 10 - PROJETO BASE/src/main/java/br/com/fiap/projetos/controller/ProjetoController.java
+++ AULA 10 - PROJETO BASE/src/main/java/br/com/fiap/projetos/controller/ProjetoController.java
@@ -1,5 +1,55 @@
 package br.com.fiap.projetos.controller;
 
+import br.com.fiap.projetos.dto.ProjetoDTO;
+import br.com.fiap.projetos.service.ProjetoService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/projetos")
+@AllArgsConstructor
+@Log
 public class ProjetoController {
-    
+
+    private final ProjetoService projetoService;
+
+    @GetMapping
+    public String listar(Model model) {
+        model.addAttribute("projetos", projetoService.listar());
+        return "projetos/lista";
+    }
+
+    @GetMapping("/novo")
+    public String novo(Model model) {
+        model.addAttribute("projeto", new ProjetoDTO());
+        return "projetos/formulario";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(@Valid @ModelAttribute("projeto") ProjetoDTO projeto, BindingResult bindingResults, Model model){
+        if (bindingResults.hasErrors()) {
+            bindingResults.getAllErrors().forEach(e-> log.info(e.toString()));
+            model.addAttribute("projeto", projeto);
+            return "projetos/formulario";
+        }
+        projetoService.criar(projeto);
+        return "redirect:/projetos";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editar(@PathVariable Long id, Model model) {
+        model.addAttribute("projeto", projetoService.findById(id));
+        return "projetos/formulario";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletar(@PathVariable Long id){
+        projetoService.apagar(id);
+        return "redirect:/projetos";
+    }
 }
--- AULA 10 - PROJETO BASE/src/main/java/br/com/fiap/projetos/dto/ProjetoDTO.java
+++ AULA 10 - PROJETO BASE/src/main/java/br/com/fiap/projetos/dto/ProjetoDTO.java
@@ -1,16 +1,19 @@
 package br.com.fiap.projetos.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjetoDTO {
     private Long id;
     private String nome;
     private String responsavel;
     private Instant dataCriacao;
 }
-
